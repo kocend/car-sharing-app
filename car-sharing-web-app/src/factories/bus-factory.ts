25f8f34cec758d87a32a18addb63ab2682237f5b
kocend <48856972+kocend@users.noreply.github.com>
2019-08-08 12:22:33 +0200

    zmodyfikowano fabryki klas

--- car-sharing-web-app/src/factories/bus-factory.ts
+++ car-sharing-web-app/src/factories/bus-factory.ts
@@ -3,6 +3,12 @@ import { VehicleFactory } from './vehicle-factory';
 import { VehicleType } from 'src/models/vehicle-type.enum';
 
 export class BusFactory extends VehicleFactory {
+    protected getMinWeight(): number {
+        return 3000;
+    }
+    protected getMaxWeight(): number {
+        return 20000;
+    }
 
     constructor(){
         super(VehicleType.Bus);
--- car-sharing-web-app/src/factories/vehicle-factory.ts
+++ car-sharing-web-app/src/factories/vehicle-factory.ts
@@ -1,2 +1,15 @@
-export class VehicleFactory {
+import { VehicleType } from 'src/models/vehicle-type.enum';
+import { Vehicle } from 'src/models/vehicle';
+
+export abstract class VehicleFactory {
+    protected abstract getMinWeight(): number;
+    protected abstract getMaxWeight(): number;
+
+    constructor( type: VehicleType){
+
+    }
+
+    protected fillBaseData(vehicle: Vehicle){
+        vehicle.weight = this.getMinWeight()+Math.random()*(this.getMaxWeight()-this.getMinWeight());
+    }
 }
